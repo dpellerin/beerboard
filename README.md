be171f257c8b6f76a054490b546bab94bc41c228
Dana Pellerin <dana@pellerin.com>
2017-12-19 21:46:57 -0800

    Prepping project setup and documentation for public display

--- README.md
+++ README.md
@@ -1,38 +1,28 @@
 ## Beer Board
-Author: Dana Pellerin
+
+by Dana Pellerin
 
 ---
 
-### Database Schema
+This is my first Node.js project. 
 
-#### Breweries
-Name | Type
---- | ---
-ID | Integer
-Username | Text
-Password | Text
-Logo | Text
-BreweryName | Text
-URL | Text
-Public | Integer
+I wrote this because I felt that existing beer tap display projects were just too much to manage. All I wanted was something to show what I have on tap in my garage brewery. I wanted it simple and easy to update.
 
-#### Beers
-Name | Type
---- | --- 
-ID | Integer
-Brewery_Name | Text
-Name | Text
-Description | Text
-Style | Text
-IBU | Real
-SRM | Real
-ABV | Real
-BreweryID | Integer
+It's written specifically for my three tap setup. I usually have one in fermentation so I made a "coming soon" section. I may eventually try to make it more flexible for other folks, but for now it was written exclusively for my needs. 
 
-#### Taps
-Name | Type
---- | --- 
-ID | Integer
-Number | Integer
-BeerID | Integer
-BreweryID | Integer
+I run this on a Raspberry Pi 3 attached to a 32" 1080p TV. 
+
+---
+
+### Setup
+
+You must create and pre-populate the database. Run the following in the db folder:
+
+`sqlite3 beerboard.sqlt < create.sql`
+
+---
+
+### To Do
+
+- Add delete and add buttons for beers
+- Allow empty taps
--- db/create.sql
+++ db/create.sql
@@ -38,16 +38,16 @@ CREATE TABLE taps (
 
 -- Beers
 INSERT INTO beers (id, brewery_name, name, description, style, ibu, srm, abv)
-VALUES (1, 'Coors', 'Red Pig IPA', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae lobortis nisi, et ultricies ligula. Morbi mollis massa vel nibh volutpat cursus vel id lorem. Phasellus vitae neque dictum, convallis justo a, aliquet nisl. Donec sagittis, neque ac porttitor faucibus, tellus diam tempus enim, sit amet tempus nisl tellus eu neque.', 'IPA', '90', '12', '7.0');
+VALUES (1, 'Coors', 'Original', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae lobortis nisi, et ultricies ligula. Morbi mollis massa vel nibh volutpat cursus vel id lorem. Phasellus vitae neque dictum, convallis justo a, aliquet nisl. Donec sagittis, neque ac porttitor faucibus, tellus diam tempus enim, sit amet tempus nisl tellus eu neque.', 'Pilsner', '20', '3', '5.0');
 
 INSERT INTO beers (id, brewery_name, name, description, style, ibu, srm, abv)
-VALUES (2, 'Budweiser', 'Douchbag Pale Ale', "Nullam luctus, tellus id faucibus blandit, augue tortor vestibulum tellus, tincidunt tristique massa velit at ipsum. Phasellus luctus sapien at rutrum efficitur. Maecenas sed auctor diam. Vivamus laoreet vitae erat ut commodo. Suspendisse hendrerit at libero nec pellentesque. Integer purus urna, vehicula vitae interdum eget, mattis ac eros. Cras dictum risus sit amet purus facilisis pellentesque.", 'Pale Ale', '60', '6', '4.5');
+VALUES (2, 'Budweiser', 'Budweiser', "Nullam luctus, tellus id faucibus blandit, augue tortor vestibulum tellus, tincidunt tristique massa velit at ipsum. Phasellus luctus sapien at rutrum efficitur. Maecenas sed auctor diam. Vivamus laoreet vitae erat ut commodo. Suspendisse hendrerit at libero nec pellentesque. Integer purus urna, vehicula vitae interdum eget, mattis ac eros. Cras dictum risus sit amet purus facilisis pellentesque.", 'Pilsner', '20', '6', '4.5');
 
 INSERT INTO beers (id, brewery_name, name, description, style, ibu, srm, abv)
-VALUES (3, 'Fucking Brewery', 'The Fat Cow', 'Vivamus sollicitudin vitae nisl et maximus. Nunc vel leo venenatis, elementum felis in, accumsan felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas semper nibh arcu, vitae iaculis nibh mollis id.', 'Stout', '30', '50', '6.5');
+VALUES (3, 'Lagunitas', 'IPA', 'Vivamus sollicitudin vitae nisl et maximus. Nunc vel leo venenatis, elementum felis in, accumsan felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas semper nibh arcu, vitae iaculis nibh mollis id.', 'IPA', '60', '12', '6.5');
 
 INSERT INTO beers (id, brewery_name, name, description, style, ibu, srm, abv)
-VALUES (4, 'Chicken Brewery', 'Some Beer', 'Curabitur ullamcorper lacus a tellus pretium ultricies. Ut consequat tincidunt nibh id commodo. Proin ut erat sapien. Sed in lacinia orci. Quisque lacinia, leo non eleifend molestie, lacus nisi lacinia elit, eu mattis nibh diam eget dolor.', 'Stout', '20', '50', '6.5');
+VALUES (4, 'Samuel Adams', 'Boston Lager', 'Curabitur ullamcorper lacus a tellus pretium ultricies. Ut consequat tincidunt nibh id commodo. Proin ut erat sapien. Sed in lacinia orci. Quisque lacinia, leo non eleifend molestie, lacus nisi lacinia elit, eu mattis nibh diam eget dolor.', 'Lager', '20', '50', '6.5');
 
 -- Taps
 INSERT INTO taps (id, number, beer_id)
